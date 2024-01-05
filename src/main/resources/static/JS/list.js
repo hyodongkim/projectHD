bce60a0992277269149a145238076e017d5e8f8b
hyodongkim <kimhd853@naver.com>
2024-01-05 15:50:00 +0900

    design

---
+++ src/main/resources/static/JS/list.js
@@ -0,0 +1,8 @@
+function fadeIn(){
+          $("header").css("opacity","1.0");
+          $(".fixBox").css("opacity","1.0");
+        }
+function fadeOut(){
+          $("header").css("opacity","1.0");
+          $(".fixBox").css("opacity","0.7");
+        ;}
