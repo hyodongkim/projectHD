c97a3e16ebbf0cb1d00a5285e9763c659ea4b312
hyodongkim <kimhd853@naver.com>
2024-03-15 16:51:04 +0900

    make Login Filter(ing)

---
+++ src/main/java/org/example/Filter/WebConfig.java
@@ -0,0 +1,28 @@
+package org.example.Filter;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<Filter>();
+        filterRegistrationBean.setFilter(new LogFilter()); // 여기서 만든 필터 클래스  등록
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean LoginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<Filter>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter()); // 여기서 만든 필터 클래스  등록
+        filterRegistrationBean.setOrder(2); // 1번인 로그필터 다음으로 수행.
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+}
